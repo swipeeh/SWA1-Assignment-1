17dc4e9c44d1a532989052da53160a412013a866
Patrik I <260010@via.dk>
2019-09-13 13:53:38 +0200

    inharitances of diagram

---
+++ Assignment_1.2.js
@@ -0,0 +1,42 @@
+//WeatherData Inharitance
+class WeatherData{
+    
+}
+class Temperature extends WeatherData{
+    convertToF(F,C) {this.F = (C*1.8)+32}
+    convertToC(F,C) {this.C = (F-32)/1.8}
+}
+class Precipitation extends WeatherData{
+    precipitationType(){}
+}
+class Wind extends WeatherData{
+    direction(){}
+}
+class CloudCoverage extends WeatherData{
+
+}
+
+//WeatherPrediction Inharitance
+class WeatherPrediction{
+    matche(data){
+        return this.data
+    }
+}
+class temperaturePrediction extends WeatherPrediction{
+    constructor(C) {this.C = C}
+    getPrediction() {return this.C}
+    setPrediction(C) {this.C = C}
+}
+class PrecipitationPrediction extends WeatherPrediction{
+    types() {}
+    matches(data) {this.data = data}
+}
+class WindPrediction extends WeatherPrediction{
+    directions(){}
+    matches (data) {this.data = data}
+}
+class CloudCoveragePrediction extends WeatherPrediction{
+
+}
+
+//WeatherFOrecast Class
